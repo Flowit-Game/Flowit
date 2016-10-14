50764a8e7608deb70c753a97cd322813c5d7446e
ByteHamster <info@bytehamster.com>
2016-10-14 18:10:13 +0200

    Using BFS instead of DFS (Fixes #27)

--- app/src/main/java/com/bytehamster/flowitgame/filler/FloodFiller.java
+++ app/src/main/java/com/bytehamster/flowitgame/filler/FloodFiller.java
@@ -7,7 +7,9 @@ import com.bytehamster.flowitgame.model.Level;
 import com.bytehamster.flowitgame.model.Modifier;
 import com.bytehamster.flowitgame.state.State;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
 
 public class FloodFiller {
     private boolean somethingWasFilled = false;
@@ -16,8 +18,6 @@ public class FloodFiller {
     private Runnable execAfter;
     private Level levelData;
     private State state;
-    private Stack<Thread> threads = new Stack<>();
-    private long lastSound = 0;
 
     public FloodFiller(Level levelData, State state, Runnable execAfter) {
         this.levelData = levelData;
@@ -28,27 +28,16 @@ public class FloodFiller {
     public void fill(final int col, final int row) {
         new Thread() {
             public void run() {
-                levelData.unvisitAll();
-                somethingWasFilled = false;
-
-                fillFrom = Modifier.EMPTY;
-                fillTo = Converter.convertColor(levelData.fieldAt(col, row).getColor());
-
                 try {
-                    floodDFS(col, row, true);
-                    while (!threads.isEmpty()) {
-                        threads.pop().join();
-                    }
+                    somethingWasFilled = false;
+                    fillFrom = Modifier.EMPTY;
+                    fillTo = Converter.convertColor(levelData.fieldAt(col, row).getColor());
+                    floodBFS(col, row);
 
                     if (!somethingWasFilled) {
-                        levelData.unvisitAll();
                         fillFrom = Converter.convertColor(levelData.fieldAt(col, row).getColor());
                         fillTo = Modifier.EMPTY;
-                        floodDFS(col, row, true);
-
-                        while (!threads.isEmpty()) {
-                            threads.pop().join();
-                        }
+                        floodBFS(col, row);
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -58,54 +47,61 @@ public class FloodFiller {
         }.start();
     }
 
-    private void startFloodDFS(final int col, final int row, final boolean isFirst) {
-        Thread t = new Thread() {
-            public void run() {
-                try {
-                    floodDFS(col, row, isFirst);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        threads.add(t);
-        t.start();
+    private class BfsNode {
+        int col, row, distance;
+        BfsNode(int col, int row, int distance) {
+            this.col = col;
+            this.row = row;
+            this.distance = distance;
+        }
+        Field getField() {
+            return levelData.fieldAt(col, row);
+        }
     }
 
-    private void floodDFS(final int col, final int row, final boolean isFirst) throws InterruptedException {
-        Field f = levelData.fieldAt(col, row);
-        if (!f.isVisited()) {
-            f.setVisited(true);
+    private void floodBFS(final int col, final int row) throws InterruptedException {
+        levelData.unvisitAll();
+        Queue<BfsNode> queue = new ArrayDeque<>();
 
-            if (f.getModifier() == fillFrom || isFirst) {
+        levelData.fieldAt(col, row).setVisited(true);
+        queue.add(new BfsNode(col, row, 0));
+        int lastDistance = 1;
 
-                if (!isFirst) {
-                    somethingWasFilled = true;
-                    f.setModifier(fillTo);
+        while(!queue.isEmpty()) {
+            BfsNode node = queue.poll();
+            for (BfsNode neighbor : getNeighbors(node)) {
+                if (!neighbor.getField().isVisited()) {
+                    neighbor.getField().setVisited(true);
 
-                    synchronized (FloodFiller.this) {
-                        if (lastSound < System.currentTimeMillis() - 80) {
-                            lastSound = System.currentTimeMillis();
+                    if(neighbor.getField().getModifier() == fillFrom) {
+                        if (lastDistance != neighbor.distance) {
+                            lastDistance = neighbor.distance;
+                            Thread.sleep(80);
                             state.playSound(R.raw.fill);
                         }
+                        neighbor.getField().setModifier(fillTo);
+                        queue.add(neighbor);
+                        somethingWasFilled = true;
                     }
-
-                    Thread.sleep(80);
-                }
-
-                if (col > 0) {
-                    startFloodDFS(col - 1, row, false);
-                }
-                if (col < 4) {
-                    startFloodDFS(col + 1, row, false);
-                }
-                if (row > 0) {
-                    startFloodDFS(col, row - 1, false);
-                }
-                if (row < 5) {
-                    startFloodDFS(col, row + 1, false);
                 }
             }
         }
+    }
+
+    private ArrayList<BfsNode> getNeighbors(BfsNode node) {
+        ArrayList<BfsNode> neighbors = new ArrayList<>();
+        if (node.col > 0) {
+            neighbors.add(new BfsNode(node.col - 1, node.row, node.distance+1));
+        }
+        if (node.col < 4) {
+            neighbors.add(new BfsNode(node.col + 1, node.row, node.distance+1));
+        }
+        if (node.row > 0) {
+            neighbors.add(new BfsNode(node.col, node.row - 1, node.distance+1));
+        }
+        if (node.row < 5) {
+            neighbors.add(new BfsNode(node.col, node.row + 1, node.distance+1));
+        }
+        return neighbors;
     }
 }
