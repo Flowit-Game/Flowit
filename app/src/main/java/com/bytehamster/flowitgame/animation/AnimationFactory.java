8bab8faaffb862cc33397436e814e79c63af719d
ByteHamster <info@bytehamster.com>
2016-10-20 16:08:30 +0200

    Renamed vars in AnimationFactory

--- app/src/main/java/com/bytehamster/flowitgame/animation/AnimationFactory.java
+++ app/src/main/java/com/bytehamster/flowitgame/animation/AnimationFactory.java
@@ -4,28 +4,28 @@ import com.bytehamster.flowitgame.object.Plane;
 
 public class AnimationFactory {
     public static void startMenuAnimationOutPressed(Plane plane) {
-        TranslateAnimation exitButtonAnimation = new TranslateAnimation(plane, Animation.DURATION_LONG, Animation.DURATION_SHORT);
-        exitButtonAnimation.setFrom(0, plane.getY());
-        exitButtonAnimation.setTo(-plane.getWidth(), plane.getY());
-        exitButtonAnimation.setHideAfter(true);
-        exitButtonAnimation.start();
+        TranslateAnimation menuAnimation = new TranslateAnimation(plane, Animation.DURATION_LONG, Animation.DURATION_SHORT);
+        menuAnimation.setFrom(0, plane.getY());
+        menuAnimation.setTo(-plane.getWidth(), plane.getY());
+        menuAnimation.setHideAfter(true);
+        menuAnimation.start();
     }
 
     public static void startMenuAnimationOut(Plane plane) {
-        TranslateAnimation exitButtonAnimation = new TranslateAnimation(plane, Animation.DURATION_SHORT, 0);
-        exitButtonAnimation.setFrom(0, plane.getY());
-        exitButtonAnimation.setTo(-plane.getWidth(), plane.getY());
-        exitButtonAnimation.setHideAfter(true);
-        exitButtonAnimation.start();
+        TranslateAnimation menuAnimation = new TranslateAnimation(plane, Animation.DURATION_SHORT, 0);
+        menuAnimation.setFrom(0, plane.getY());
+        menuAnimation.setTo(-plane.getWidth(), plane.getY());
+        menuAnimation.setHideAfter(true);
+        menuAnimation.start();
     }
 
     public static void startMenuAnimationEnter(Plane plane, int delay) {
         plane.setVisible(true);
         plane.setX(-plane.getWidth());
-        TranslateAnimation exitButtonAnimation = new TranslateAnimation(plane, Animation.DURATION_LONG, delay);
-        exitButtonAnimation.setFrom(-plane.getWidth(), plane.getY());
-        exitButtonAnimation.setTo(0, plane.getY());
-        exitButtonAnimation.start();
+        TranslateAnimation menuAnimation = new TranslateAnimation(plane, Animation.DURATION_LONG, delay);
+        menuAnimation.setFrom(-plane.getWidth(), plane.getY());
+        menuAnimation.setTo(0, plane.getY());
+        menuAnimation.start();
     }
 
     public static void startScaleShow(Plane plane) {
