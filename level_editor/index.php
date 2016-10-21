557af880b3eb85daa5046159901eddd878f42082
ByteHamster <info@bytehamster.com>
2016-10-21 17:35:28 +0200

    Added arrow navigation for editor

--- level_editor/index.php
+++ level_editor/index.php
@@ -111,6 +111,44 @@ if (!isset($_SESSION["level_data"]) && isset($_GET["cols"]) && isset($_GET["rows
                 margin:auto;
             }
         </style>
+        <script>
+            function findGetParameter(parameterName) {
+                var result = null,
+                    tmp = [];
+                var items = location.search.substr(1).split("&");
+                for (var index = 0; index < items.length; index++) {
+                    tmp = items[index].split("=");
+                    if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+                }
+                return result;
+            }
+            function param(paramName, offset) {
+                return Number(findGetParameter(paramName)) + offset;
+            }
+            
+            document.onkeydown = checkKey;
+            function checkKey(e) {
+                e = e || window.event;
+                if (e.keyCode == '38') {
+                    // up arrow
+                    window.location.href="./?action=edit&c="+param("c",  0)+"&r="+param("r", -1);
+                    e.preventDefault();
+                } else if (e.keyCode == '40') {
+                    // down arrow
+                    window.location.href="./?action=edit&c="+param("c",  0)+"&r="+param("r", +1);
+                    e.preventDefault();
+                } else if (e.keyCode == '37') {
+                   // left arrow
+                    window.location.href="./?action=edit&c="+param("c", -1)+"&r="+param("r",  0);
+                    e.preventDefault();
+                } else if (e.keyCode == '39') {
+                   // right arrow
+                    window.location.href="./?action=edit&c="+param("c", +1)+"&r="+param("r",  0);
+                    e.preventDefault();
+                }
+
+            }
+        </script>
     </head>
     <body>
         <div>
