77459ab21ec3a9205d120a60f9fab6e35bc684b4
ByteHamster <info@bytehamster.com>
2016-10-21 21:18:54 +0200

    Updated deprecated SoundPool

--- app/src/main/java/com/bytehamster/flowitgame/SoundPool.java
+++ app/src/main/java/com/bytehamster/flowitgame/SoundPool.java
@@ -2,7 +2,9 @@ package com.bytehamster.flowitgame;
 
 import android.app.Activity;
 import android.content.Context;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
+import android.os.Build;
 import android.util.SparseIntArray;
 
 public class SoundPool {
@@ -10,19 +12,31 @@ public class SoundPool {
 	private final SparseIntArray items = new SparseIntArray();
 	private final Context myContext;
 	
-	public SoundPool(Activity a) {
+	SoundPool(Activity a) {
 		a.setVolumeControlStream(AudioManager.STREAM_MUSIC);
 		myContext = a;
-        pool = new android.media.SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+
+		if (Build.VERSION.SDK_INT < 21) {
+			//noinspection deprecation
+			pool = new android.media.SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+		} else {
+			android.media.SoundPool.Builder builder = new android.media.SoundPool.Builder();
+			builder.setAudioAttributes(new AudioAttributes.Builder()
+					.setUsage(AudioAttributes.USAGE_GAME)
+					.setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+					.build());
+			builder.setMaxStreams(3);
+			pool = builder.build();
+		}
+
         pool.setOnLoadCompleteListener(new android.media.SoundPool.OnLoadCompleteListener() {
             @Override
-			public void onLoadComplete(android.media.SoundPool soundPool, int sampleId, int status)
-			{
+			public void onLoadComplete(android.media.SoundPool soundPool, int sampleId, int status) {
 			}
         });
         
 	}
-	public void loadSound(int ResID){
+	void loadSound(int ResID){
 		items.put(ResID, pool.load(myContext, ResID, 1));
 	}
 
