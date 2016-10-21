07f181a526279a04beccf5c2b2d15b584c2cb9de
ByteHamster <info@bytehamster.com>
2016-10-21 18:13:50 +0200

    Fixed arrow keys destroying level editor

--- level_editor/index.php
+++ level_editor/index.php
@@ -43,20 +43,25 @@ $COLOR_MAPPING2 = array(
 if (@$_GET["action"] == "restart") {
     unset($_SESSION["level_data"]);
 } else if (@$_GET["action"] == "save_color") {
-    $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["dest_color"] = $_GET["color"];
+    if(isset($_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["dest_color"]))
+        $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["dest_color"] = $_GET["color"];
     $_GET["action"] = "edit";
 } else if (@$_GET["action"] == "save_preset") {
-    $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["color"] = $_GET["color"];
+    if(isset($_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["color"]))
+        $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["color"] = $_GET["color"];
     $_GET["action"] = "edit";
 } else if (@$_GET["action"] == "save_type") {
-    $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["type"] = $_GET["type"];
-    if($_GET["type"] == 0)
-        $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["dest_color"] = $COLOR_RED;
-    else
-        $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["dest_color"] = $COLOR_BLACK;
+    if(isset($_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["type"]))
+        $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["type"] = $_GET["type"];
+    if(isset($_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["dest_color"]))
+        if($_GET["type"] == 0)
+            $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["dest_color"] = $COLOR_RED;
+        else
+            $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["dest_color"] = $COLOR_BLACK;
     $_GET["action"] = "edit";
 } else if (begins_with(@$_GET["action"], "save_")) {
-    $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["action"] = str_replace("save_", "", $_GET["action"]);
+    if(isset($_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["action"]))
+        $_SESSION["level_data"][$_GET["r"]][$_GET["c"]]["action"] = str_replace("save_", "", $_GET["action"]);
     $_GET["action"] = "edit";
 }
 
