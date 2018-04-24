e9b023067ebc2b68f4c60b0b81f2e6992158d32f
ByteHamster <info@bytehamster.com>
2018-04-24 20:06:41 +0200

    Improved level anomaly detection

--- app/src/main/java/com/bytehamster/flowitgame/util/PackRanges.java
+++ app/src/main/java/com/bytehamster/flowitgame/util/PackRanges.java
@@ -1,39 +1,72 @@
 package com.bytehamster.flowitgame.util;
 
+import java.util.ArrayList;
+
 public class PackRanges {
     private PackRanges() {
 
     }
 
-    public static int[][] RANGE_1 = new int[][]{
+    public static final int[][] RANGE_1 = new int[][]{
             new int[]{0, 24},
             new int[]{75, 99},
     };
-    public static int[][] RANGE_2 = new int[][]{
+    public static final int[][] RANGE_2 = new int[][]{
             new int[]{25, 49}
     };
-    public static int[][] RANGE_3 = new int[][]{
+    public static final int[][] RANGE_3 = new int[][]{
             new int[]{50, 74}
     };
 
+
+    private static final ArrayList<int[]> ANOMALIES = calculateAnomalies();
+
+    private static ArrayList<int[]> calculateAnomalies() {
+        ArrayList<int[]> result = new ArrayList<>();
+        result.addAll(calculateAnomalies(RANGE_1));
+        result.addAll(calculateAnomalies(RANGE_2));
+        result.addAll(calculateAnomalies(RANGE_3));
+        return result;
+    }
+
+    private static ArrayList<int[]> calculateAnomalies(int[][] pack) {
+        ArrayList<int[]> result = new ArrayList<>();
+        for (int i = 0; i < pack.length - 1; i++) {
+            result.add(new int[]{pack[i][1], pack[i+1][0]});
+        }
+        return result;
+    }
+
+    private static int firstOf(int [][] pack) {
+        return pack[0][0];
+    }
+
     public static boolean isFirstInPack(int level) {
-        return level == 0 || level == 25 || level == 50;
+        return level == firstOf(RANGE_1) || level == firstOf(RANGE_2) || level == firstOf(RANGE_3);
+    }
+
+    private static int lastOf(int [][] pack) {
+        return pack[pack.length - 1][1];
     }
 
     public static boolean isLastInPack(int level) {
-        return level == 99 || level == 49 || level == 74;
+        return level == lastOf(RANGE_1) || level == lastOf(RANGE_2) || level == lastOf(RANGE_3);
     }
 
     public static int previousLevel(int level) {
-        if (level == 75) {
-            return 24;
+        for (int[] anomaly : ANOMALIES) {
+            if (anomaly[1] == level) {
+                return anomaly[0];
+            }
         }
         return level - 1;
     }
 
     public static int nextLevel(int level) {
-        if (level == 24) {
-            return 75;
+        for (int[] anomaly : ANOMALIES) {
+            if (anomaly[0] == level) {
+                return anomaly[1];
+            }
         }
         return level + 1;
     }
