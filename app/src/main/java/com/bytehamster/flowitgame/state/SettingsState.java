67a62b13d627607ef702e6400c379a4c9cad3e6c
ByteHamster <info@bytehamster.com>
2018-05-16 22:43:03 +0200

    Added consent button

--- app/src/main/java/com/bytehamster/flowitgame/state/SettingsState.java
+++ app/src/main/java/com/bytehamster/flowitgame/state/SettingsState.java
@@ -20,6 +20,8 @@ public class SettingsState extends State {
     private Plane volumeOff;
     private Plane volumeOn;
     private Plane tutorialButton;
+    private Plane volumeButton;
+    private Plane consentButton;
 
     private SettingsState() {
 
@@ -36,23 +38,31 @@ public class SettingsState extends State {
     protected void initialize(GLRenderer glRenderer) {
         float menuEntriesWidth = glRenderer.getWidth() * 0.75f;
         float menuEntriesHeight = menuEntriesWidth / 6;
+        float menuEntriesAvailableSpace = getScreenHeight() - getAdHeight();
+        float menuEntriesStartY = getScreenHeight() - (menuEntriesAvailableSpace - 4 * menuEntriesHeight) / 2;
 
-        volumeOn  = ObjectFactory.createSingleBox(0, 15, getScreenWidth()/4);
-        volumeOn.setVisible(false);
-        volumeOn.setX(getScreenWidth()/8*3);
-        volumeOn.setY((getScreenHeight() + getAdHeight() - volumeOn.getHeight())/2 + menuEntriesHeight);
-        glRenderer.addDrawable(volumeOn);
-        volumeOff = ObjectFactory.createSingleBox(1, 15, getScreenWidth()/4);
-        volumeOff.setVisible(false);
-        volumeOff.setX(getScreenWidth()/8*3);
-        volumeOff.setY((getScreenHeight() + getAdHeight() - volumeOff.getHeight())/2 + menuEntriesHeight);
-        glRenderer.addDrawable(volumeOff);
-
-        float menuEntriesStartY = volumeOff.getY() - menuEntriesHeight * 3;
+        TextureCoordinates coordinatesVolume = TextureCoordinates.getFromBlocks(6, 13, 12, 14);
+        volumeButton = new Plane(-menuEntriesWidth, menuEntriesStartY, menuEntriesWidth, menuEntriesHeight, coordinatesVolume);
+        glRenderer.addDrawable(volumeButton);
 
         TextureCoordinates coordinatesTutorial = TextureCoordinates.getFromBlocks(6, 12, 12, 13);
-        tutorialButton = new Plane(-menuEntriesWidth, menuEntriesStartY, menuEntriesWidth, menuEntriesHeight, coordinatesTutorial);
+        tutorialButton = new Plane(-menuEntriesWidth, volumeButton.getY() - 2 * menuEntriesHeight, menuEntriesWidth, menuEntriesHeight, coordinatesTutorial);
         glRenderer.addDrawable(tutorialButton);
+
+        TextureCoordinates coordinatesConsent = TextureCoordinates.getFromBlocks(6, 14, 12, 15);
+        consentButton = new Plane(-menuEntriesWidth, tutorialButton.getY() - 2 * menuEntriesHeight, menuEntriesWidth, menuEntriesHeight, coordinatesConsent);
+        glRenderer.addDrawable(consentButton);
+
+        volumeOn  = ObjectFactory.createSingleBox(0, 15, menuEntriesHeight);
+        volumeOn.setVisible(false);
+        volumeOn.setX(menuEntriesWidth);
+        volumeOn.setY(volumeButton.getY());
+        glRenderer.addDrawable(volumeOn);
+        volumeOff = ObjectFactory.createSingleBox(1, 15, menuEntriesHeight);
+        volumeOff.setVisible(false);
+        volumeOff.setX(menuEntriesWidth);
+        volumeOff.setY(volumeButton.getY());
+        glRenderer.addDrawable(volumeOff);
     }
 
     @Override
@@ -67,7 +77,9 @@ public class SettingsState extends State {
         scaleAnimation.setTo(1);
         scaleAnimation.start();
 
-        AnimationFactory.startMenuAnimationEnter(tutorialButton, (int) (2.0f * Animation.DURATION_SHORT));
+        AnimationFactory.startMenuAnimationEnter(volumeButton, (int) (2.0f * Animation.DURATION_SHORT));
+        AnimationFactory.startMenuAnimationEnter(tutorialButton, (int) (2.5f * Animation.DURATION_SHORT));
+        AnimationFactory.startMenuAnimationEnter(consentButton, (int) (3.0f * Animation.DURATION_SHORT));
     }
 
     @Override
@@ -84,6 +96,9 @@ public class SettingsState extends State {
         } else {
             AnimationFactory.startMenuAnimationOut(tutorialButton);
         }
+
+        AnimationFactory.startMenuAnimationOut(volumeButton);
+        AnimationFactory.startMenuAnimationOut(consentButton);
     }
 
     @Override
@@ -100,7 +115,7 @@ public class SettingsState extends State {
     @Override
     public void onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            if (volumeOn.collides(event, getScreenHeight())) {
+            if (volumeOn.collides(event, getScreenHeight()) || volumeButton.collides(event, getScreenHeight())) {
                 playSound(R.raw.click);
                 boolean newVolume = ! getPreferences().getBoolean("volumeOn", true);
                 getPreferences().edit().putBoolean("volumeOn", newVolume).apply();
@@ -109,6 +124,10 @@ public class SettingsState extends State {
             } else if (tutorialButton.collides(event, getScreenHeight())) {
                 nextState = TutorialState.getInstance();
                 playSound(R.raw.click);
+            } else if (consentButton.collides(event, getScreenHeight())) {
+                nextState = MainMenuState.getInstance();
+                playSound(R.raw.click);
+                // TODO
             }
         }
     }
